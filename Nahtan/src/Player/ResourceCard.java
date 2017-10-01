8fd2fb75646a0feec84288d8598d852bab098484
NTRobinson <robinson844@gmail.com>
2017-09-30 21:26:49 -0500

    Update ResourceCard.java

--- Nahtan/src/Player/ResourceCard.java
+++ Nahtan/src/Player/ResourceCard.java
@@ -1,6 +1,18 @@
 package Player;
 
+import Board.ResourceType;
+
 public class ResourceCard
 {
-
+	ResourceType type;
+	
+	public ResourceCard(ResourceType type)
+	{
+		this.type = type;
+	}
+	
+	public ResourceType getType()
+	{
+		return this.type;
+	}
 }

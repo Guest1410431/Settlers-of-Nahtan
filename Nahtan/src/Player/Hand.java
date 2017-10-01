bd553618453e33e8544b953e9c19f3f6e6eebd6b
NTRobinson <robinson844@gmail.com>
2017-09-30 21:20:06 -0500

    Update Hand.java

--- Nahtan/src/Player/Hand.java
+++ Nahtan/src/Player/Hand.java
@@ -1,6 +1,30 @@
 package Player;
 
+import java.util.ArrayList;
+
+import Board.ResourceType;
+
 public class Hand
 {
+	private ArrayList<ResourceCard> hand;
+	private int clay;
+	private int ore;
+	private int lumber;
+	private int wheat;
+	private int sheep;
 	
+	public Hand()
+	{
+		clay = 0;
+		ore= 0;
+		lumber = 0;
+		wheat = 0;
+		sheep = 0;
+		
+	}
+	
+	public int howMany(ResourceType type)
+	{
+		
+	}
 }
